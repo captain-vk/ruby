b6091a1f4901289b5765afa6f0130c46107833d9
captain-vk <vk2609@rambler.ru>
2019-08-13 18:49:13 +0300

    Ruby

--- lesson_oop2/main.rb
+++ lesson_oop2/main.rb
@@ -19,12 +19,13 @@ def main_menu
   puts "Нажмите 1 чтобы, Создать станцию"
   puts "Нажмите 2 чтобы, Создать поезд"
   puts "Нажмите 3 для создания маршрута"
-  puts "Нажмите 4 для управлениями станциями в маршруте"
-  puts "Нажмите 5 чтобы, Назначить маршрут поезду"
-  puts "Нажмите 6 чтобы, Добавлять вагоны к поезду"
-  puts "Нажмите 7 чтобы, Отцеплять вагоны от поезда"
-  puts "Нажмите 8 чтобы, Перемещать поезд по маршруту вперед и назад"
-  puts "Нажмите 9 чтобы, Просматривать список станций и список поездов на станции"
+  puts "Нажмите 4 для добавления станции в маршруте"
+  puts "Нажмите 5 для удаления станции в маршруте"
+  puts "Нажмите 6 чтобы, Назначить маршрут поезду"
+  puts "Нажмите 7 чтобы, Добавлять вагоны к поезду"
+  puts "Нажмите 8 чтобы, Отцеплять вагоны от поезда"
+  puts "Нажмите 9 чтобы, Перемещать поезд по маршруту вперед и назад"
+  puts "Нажмите 10 чтобы, Просматривать список станций и список поездов на станции"
   puts "Нажмите 0 для выхода"
   input = gets.chomp.to_i
 
@@ -36,16 +37,18 @@ def main_menu
   when 3
     create_route
   when 4
-    control_stations_on_route
+    add_stations_on_route
   when 5
-    get_route_to_train
+    delete_stations_on_route
   when 6
-    add_wagon
+    get_route_to_train
   when 7
-    delete_wagon
+    add_wagon
   when 8
-    move_train
+    delete_wagon
   when 9
+    move_train
+  when 10
     list_trains_and_stations
   when 0
     exit
@@ -88,13 +91,34 @@ end
     routes << Route.new(first, last)
   end
 
-  def control_stations_on_route
-    puts "Выберите маршрут для редактирования - введите номер"
-    routes.each.with_index { |route, index| puts "#{index} - #{route.stations.name}" }
-
-
+  def add_stations_on_route
+    routes.each.with_index { |route, index| puts "#{index} - #{route.stations.map { |station| station.name} }"  }
+    puts "Выберите маршрут в который требуется добавить станцию - введите номер"
+    number_route = gets.chomp.to_i
+    current_route = routes[number_route]
+    puts "Введите номер промежуточной станции"
+    stations.each.with_index { |station, index| puts "#{index} - #{station.name}" }
+    number_station = gets.chomp.to_i
+    current_route.add_station(stations[number_station])
   end
 
+  def delete_stations_on_route
+    routes.each.with_index { |route, index| puts "#{index} - #{route.stations.map { |station| station.name} }"  }
+    puts "Выберите маршрут в котором требуется удалить станцию - введите номер"
+    number_route = gets.chomp.to_i
+    current_route = routes[number_route]
+    puts "Введите номер промежуточной станции"
+    stations.each.with_index { |station, index| puts "#{index} - #{station.name}" }
+    number_station = gets.chomp.to_i
+    current_route.delete_station(stations[number_station])
+    end
+
+    def add_wagon
+      trains.each.with_index { |train, index| puts "#{index} - #{train.number}:#{train.type}" }
+      puts "Выберите поезд для добавления к нему вагона"
+
+
+    end
     #puts "Если требуется добавить промежуточную станцию"
 
 
