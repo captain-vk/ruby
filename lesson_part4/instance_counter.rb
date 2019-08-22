aba92f3576ec1e3357f5b21a0c4f80b6cd4c2d71
captain-vk <vk2609@rambler.ru>
2019-08-22 15:53:23 +0300

    Ruby

--- lesson_part4/instance_counter.rb
+++ lesson_part4/instance_counter.rb
@@ -4,18 +4,18 @@ module InstanceCounter
     base.send :include, InstanceMethods
   end
 
-  module ClassMethods
-    def print_string
-      @@instances
-      puts "I am here"
-    end
+module ClassMethods
+  def print_string
+    @@instances
+    puts "I am here"
+  end
 
-  module InstanceMethods
-    attr_accessor :instances
-	@@instances ||= 0
+module InstanceMethods
+  attr_accessor :instances
+    @@instances ||= 0
     def print_class
-      @@instances += 1
-      puts self.class.class_variable_get(:@@instances)
+    @@instances += 1
+    puts self.class.class_variable_get(:@@instances)
     end
   end
 end
