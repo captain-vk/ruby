d3baadbb8a095677bd5c0bc48981cf38197bcc8c
CaptainK <vk2609@rambler.ru>
2019-08-22 23:23:25 +0300

    Ruby

---
+++ lesson_part5/station.rb
@@ -0,0 +1,34 @@
+require_relative 'company'
+require_relative 'instance_counter'
+
+class Station
+  include Company
+  include InstanceCounter
+  
+  attr_reader :trains, :name
+  @@stations = []
+
+  def initialize(name)
+    @name = name
+    @trains = []
+    @@stations << self
+    register_instance  
+  end
+
+  def self.all
+    @@stations
+  end
+
+  def add_train(number)
+    @trains << number
+  end
+
+  def list_of_trains_by_type(type)
+    @trains.select { |train| train.type == type }
+  end
+
+  def send_trains(number)
+    @trains.delete(number)
+  end
+
+end
---
+++ lesson_part5/train.rb
@@ -0,0 +1,74 @@
+require_relative 'company'
+require_relative 'instance_counter'
+
+class Train
+  include Company
+  include InstanceCounter
+
+  attr_reader :number, :type, :current_station, :speed, :route, :wagons
+  @@trains = []
+
+  def initialize(number, type)
+    @number = number
+    @type = type
+    @speed = 0
+    @wagons = []
+    @@trains[number] = self
+    register_instance  
+  end
+
+  def self.find(number)
+    @@trains[number]
+  end
+
+  def increase_speed(speed)
+    @speed += speed
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def add_wagon(wagon)
+    @wagons << wagon if @speed == 0
+  end
+
+  def delete_wagon
+    @wagons.pop if ((@speed == 0) && (@wagons.size > 0))
+  end
+
+  def count_wagons
+    @wagons.size
+  end
+
+  def add_route(route)
+    @route = route
+    @current_station = @route.stations[0]
+    @current_station.add_train(self)
+  end
+
+  def move_forward
+    @current_station = next_station
+  end
+
+  def move_back
+    @current_station = previous_station
+  end
+
+  def show_current_station
+    @route.stations.each do |station|
+    @current_station = station if station.trains.include?(self)
+  end
+end
+  private
+
+  attr_writer :speed
+
+  def next_station
+    @route.stations[@route.stations.index(@current_station) + 1] if @current_station != @route.stations.last
+  end
+
+  def previous_station
+    @current_station = @route.stations[@route.stations.index(@current_station) - 1] if @current_station != @route.stations.first
+  end
+end
