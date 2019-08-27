d8b10766e002eb7f1db03a846c6e28a5acacd2f3
captain-vk <vk2609@rambler.ru>
2019-08-27 19:11:05 +0300

    Ruby

---
+++ lesson_part7/cargo_wagon.rb
@@ -0,0 +1,28 @@
+require_relative 'company'
+class CargoWagon
+  include Company
+  attr_reader :type, :number, :total_volume, :free_volume
+
+  @@instances = 0
+
+  def initialize(total_volume, type)
+  	@@instances += 1
+    @number = @@instances
+  	@total_volume = total_volume
+  	@free_volume = @total_volume
+    @type = type    
+  end 
+
+  def takes_volume(volume)
+  	raise "Не хватит места" if @free_volume < volume
+  	@free_volume -= volume
+  end
+
+  def busy_volume
+  	@total_volume - @free_volume
+  end
+
+  def free_volume
+  	@free_volume
+  end 
+end
