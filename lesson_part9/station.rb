35d242d201198d5f2efd8b1d183389ffe6f0bc8d
CaptainK <vk2609@rambler.ru>
2019-09-07 23:03:27 +0300

    Ruby

--- lesson_part9/station.rb
+++ lesson_part9/station.rb
@@ -2,19 +2,25 @@
 
 require_relative 'company'
 require_relative 'instance_counter'
+require_relative 'validation'
+
 
 class Station
   include Company
   include InstanceCounter
+  include Validation
+
 
   attr_reader :trains, :name
   @@stations = []
+  validate :name, :presence
+
 
   def initialize(name)
     @name = name
     @trains = []
     @@stations << self
-    validate!
+    # validate!
     register_instance
   end
 
@@ -22,11 +28,11 @@ class Station
     @@stations
   end
 
-  def valid?
-    validate!
-  rescue StandardError
-    false
-  end
+  # def valid?
+  #   validate!
+  # rescue StandardError
+  #   false
+  # end
 
   def self.all
     @@stations
@@ -48,13 +54,13 @@ class Station
     @trains.delete(number)
   end
 
-  protected
+  # protected
 
-  def validate!
-    validate_number!
-  end
+  # def validate!
+  #   validate_number!
+  # end
 
-  def validate_number!
-    raise 'Название станции не корректно' if name.length < 5
-  end
+  # def validate_number!
+  #   raise 'Название станции не корректно' if name.length < 5
+  # end
 end
--- lesson_part9/validation.rb
+++ lesson_part9/validation.rb
@@ -2,23 +2,55 @@
 # frozen_string_literal: true
 
 module Validation
-  def validate_presence(attr)
-    raise "..." if attr == nil || attr.empty?
+
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
   end
 
-  def validate_format(attr, format)
-    raise "..." if attr.to_s !~ format    
+  module CLassMethods
+    def validate(var, type, arg)
+      @validations ||= []
+      @validations.push(type: type, arg: arg, var: var)
+    end
+
+    def validations
+      @validations
+    end
   end
 
-  def validate_type(attr, format)
-    raise "..." if attr.to_s !~ format    
-  end  
+  module InstanceMethods
+    def validate!
+      self.class.validations.each do |validation|
+        send("validate_#{validation[:type]}".to_sym, validation[:var], validation[:arg])
+      end
+    end
 
+    def valid?
+      validate!
+      true
+    rescue
+      false
+    end
 
+    private
 
+    def validate_presence(attr, _)
+      raise "Строка пустая или nil" if attr == nil || attr.empty?
+    end
 
-class Test
-  extend Accessors
-  attr_accessor_with_history :my_attr, :a, :b, :c
-  strong_attr_accessor :l, Integer
+    def validate_format(attr, format)
+      raise "Не соответствует формату" if attr.to_s !~ format    
+    end
+
+    def validate_type(attr, format)
+      raise "Не соответствует заданному классу" if attr.to_s !~ format    
+    end  
+  end
 end
+
+# class Test
+#   extend Accessors
+#   attr_accessor_with_history :my_attr, :a, :b, :c
+#   strong_attr_accessor :l, Integer
+# end
